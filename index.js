75e01c9e9b0e47ac6779f62d187c71aa931c7d31
Zhu Wensheng <zhuwensheng@JJJX.local>
2021-03-31 09:35:33 +0800

    本地端口调整

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ if (typeof web3 !== 'undefined')
 	web3 = new Web3(web3.currentProvider);  
 else
 {	
-	// web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+	// web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8101"));
     web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.1.126:8101"));
  }    
 if(!web3.isConnected()) 
